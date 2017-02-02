d7a2b72dea1930f572ed19f51de29027068f44aa
bojkov@mbconsalting.ru <bojkov@mbconsalting.ru>
2017-02-02 14:35:49 +0300

    Исправлены ошибки по комментариям.

--- spec/controllers/answers_controller/create_spec.rb
+++ spec/controllers/answers_controller/create_spec.rb
@@ -1,26 +1,33 @@
 RSpec.describe AnswersController, type: :controller do
-  let(:question) { create(:question) }
-  let(:answer) { create(:answer) }
+  let!(:question) { create(:question) }
+  let!(:answer_valid_params) { {answer: attributes_for(:answer), question_id: question} }
+  let!(:answer_invalid_params) { {answer: attributes_for(:answer, :invalid), question_id: question} }
+
+  let!(:create_post) do
+    Proc.new do |params = answer_valid_params |
+      post :create, params: params
+    end
+  end
 
   describe 'POST #create' do
     context 'with valid attributes' do
       it 'save new answer for the question in the database' do
-        expect { post :create, params: { answer: attributes_for(:answer), question_id: question }}.to change(Answer, :count).by(1)
+        expect { create_post.call }.to change(question.answers, :count).by(1)
       end
 
       it 'redirect to question show view' do
-        post :create, params: { answer: attributes_for(:answer), question_id: question }
+        create_post.call
         expect(response).to redirect_to question_path(question)
       end
     end
 
     context 'with invalid attributes' do
       it 'does not save the new answer for question in the database' do
-        expect { post :create, params: { answer: attributes_for(:answer, :invalid), question_id: question } }.to_not change(Answer, :count)
+        expect { create_post.call(answer_invalid_params) }.to_not change(question.answers, :count)
       end
 
       it 're-render answer new view' do
-        post :create, params: { answer: attributes_for(:answer, :invalid), question_id: question }
+        create_post.call(answer_invalid_params)
         expect(response).to render_template :new
       end
     end
--- spec/controllers/questions_controller/create_spec.rb
+++ spec/controllers/questions_controller/create_spec.rb
@@ -1,25 +1,32 @@
 RSpec.describe QuestionsController, type: :controller do
-  let(:question) { create(:question) }
+  let!(:question_valid_params) { {question: attributes_for(:question)} }
+  let!(:question_invalid_params) { {question: attributes_for(:question, :invalid)} }
+
+  let!(:create_post) do
+    Proc.new do |params = question_valid_params |
+      post :create, params: params
+    end
+  end
 
   describe 'POST #create' do
     context 'with valid attributes' do
       it 'saves the new question in the database' do
-        expect { post :create, params: { question: attributes_for(:question) } }.to change(Question, :count).by(1)
+        expect { create_post.call }.to change(Question, :count).by(1)
       end
 
       it 'redirects to show view' do
-        post :create, params: { question: attributes_for(:question) }
+        create_post.call
         expect(response).to redirect_to question_path(assigns(:question))
       end
     end
 
     context 'with invalid attributes' do
       it 'does not save the new question in the database' do
-        expect { post :create, params: { question: attributes_for(:question, :invalid) } }.to_not change(Question, :count)
+        expect { create_post.call(question_invalid_params) }.to_not change(Question, :count)
       end
 
       it 're-renders new view' do
-        post :create, params: { question: attributes_for(:question, :invalid) }
+        create_post.call(question_invalid_params)
         expect(response).to render_template :new
       end
     end
