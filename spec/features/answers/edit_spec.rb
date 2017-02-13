288296af35266647ddf82dd6dc446eaf881a1598
mbconsalting <bojkov@mbconsalting.ru>
2017-02-13 23:07:11 +0300

    Написаны приемочные тесты покрывающие редактирование вопроса пользователем.

--- spec/features/answers/edit_spec.rb
+++ spec/features/answers/edit_spec.rb
@@ -10,54 +10,54 @@ feature 'Edit answer', %q{
   given(:second_user) { create(:user) }
   given!(:question) { create(:question) }
 
-  scenario 'Unauthenticated user not sees edit link for answers', js: true do
-    create(:answer, question: question, user: user)
-    create(:answer, question: question, user: second_user)
+  describe 'with user answer' do
+    let!(:answer) { create(:answer, question: question, user: user) }
 
-    visit question_path(question)
+    scenario 'Unauthenticated user not sees edit link for answers' do
+      create(:answer, question: question, user: second_user)
 
-    within '.social-footer' do
-      expect(page).to_not have_content t('common.button.edit')
-    end
-  end
+      visit question_path(question)
 
-  scenario 'can edit his answer', js: true do
-    create(:answer, question: question, user: user)
-
-    login_as(user, scope: :user, run_callbacks: false)
-
-    visit question_path(question)
-
-    #save_screenshot('/home/dv1/development/stackforum/log/img0.png')
-
-    within '.social-footer' do
-      find('#edit-answer', match: :first).click
+      within '.social-footer' do
+        expect(page).to_not have_content t('common.button.edit')
+      end
     end
 
-    sleep 1
+    scenario 'can edit his answer', js: true do
+      login_as(user, scope: :user, run_callbacks: false)
 
-    page.execute_script("$('.wysihtml5-sandbox')[0].contentWindow.document.body.innerHTML='Test answer';")
+      visit question_path(question)
 
-    #save_screenshot('/home/dv1/development/stackforum/log/img2.png')
+      #save_screenshot('/home/dv1/development/stackforum/log/img0.png')
 
-    page.find('#ready-button', match: :first).click()
+      within '.social-footer' do
+        find('#edit-answer', match: :first).click
+      end
 
-    within '.social-footer' do
-      expect(page).to have_content 'Test answer'
+      sleep 1
+
+      page.execute_script("$('.wysihtml5-sandbox')[0].contentWindow.document.body.innerHTML='Test answer';")
+
+      #save_screenshot('/home/dv1/development/stackforum/log/img2.png')
+
+      find('#ready-button', match: :first).click()
+
+      within '.social-footer' do
+        expect(page).to have_content 'Test answer'
+      end
+
+      expect(current_path).to eq question_path(question)
+      expect(page).not_to have_content t('common.button.ready')
     end
 
-    expect(current_path).to eq question_path(question)
-    expect(page).not_to have_content t('common.button.ready')
-  end
+    scenario 'sees link to edit answer' do
+      login_as(user, scope: :user)
 
-  scenario 'sees link to edit answer', js: true do
-    create(:answer, question: question, user: user)
-    login_as(user, scope: :user, run_callbacks: false)
+      visit question_path(question)
 
-    visit question_path(question)
-
-    within '.social-footer' do
-      expect(page).to have_content t('common.button.edit')
+      within '.social-footer' do
+        expect(page).to have_content t('common.button.edit')
+      end
     end
   end
 
--- spec/features/questions/edit_spec.rb
+++ spec/features/questions/edit_spec.rb
@@ -1,14 +1,67 @@
+require_relative '../acceptance_helper'
+
 feature 'Edit question', %q{
   In order to fix mistake
   As an author
   I want to be able edit question
 } do
 
-  scenario 'Unauthenticated user tries edit question'
+  given!(:user) { create(:user) }
+  given!(:second_user) { create(:user) }
+  given!(:question) { create(:question, user: user) }
+  given!(:other_user_question) { create(:question, user: second_user) }
+
+  scenario 'Unauthenticated user not see link to edit question' do
+    visit question_path(question)
+
+    expect(page).not_to have_content t('common.button.edit')
+  end
+
   describe 'Authenticated user' do
-    scenario 'sees link to edit his question'
-    scenario 'not sees link to edit other user''s question'
-    scenario 'tries edit his question'
-    scenario 'tries to edit other user''s question'
+    before do
+      login_as(user, scope: :user, run_callbacks: false)
+    end
+
+    describe 'as the question owner' do
+      before do
+        visit question_path(question)
+      end
+
+      scenario 'sees link to edit' do
+        within '.social-action' do
+          expect(page).to have_content t('common.button.edit')
+        end
+      end
+
+      scenario 'can edit body' do
+        within '.social-action' do
+          find('#edit-question', match: :first).click
+        end
+
+        sleep 1
+        page.execute_script("$('.wysihtml5-sandbox')[0].contentWindow.document.body.innerHTML='Test edit question';")
+
+        find('#ready-button', match: :first).click()
+
+        within '.social-body' do
+          expect(page).to have_content 'Test edit question'
+        end
+
+        expect(current_path).to eq question_path(question)
+        expect(page).not_to have_content t('common.button.ready')
+      end
+    end
+
+    describe 'not as the question owner' do
+      before do
+        visit question_path(other_user_question)
+      end
+
+      scenario 'not sees link to edit' do
+        within '.social-action' do
+          expect(page).not_to have_content t('common.button.edit')
+        end
+      end
+    end
   end
 end
