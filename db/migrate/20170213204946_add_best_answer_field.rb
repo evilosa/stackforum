7a6fe49e2826cb7772d6236e82aa802cf8090601
bojkov@mbconsalting.ru <bojkov@mbconsalting.ru>
2017-02-17 13:01:56 +0300

    Добавил в базу композитный ключ, и значение по умолчанию.

--- db/migrate/20170213204946_add_best_answer_field.rb
+++ db/migrate/20170213204946_add_best_answer_field.rb
@@ -1,5 +1,5 @@
 class AddBestAnswerField < ActiveRecord::Migration[5.0]
   def change
-    add_column :answers, :best, :boolean, index: true, default: false
+    add_column :answers, :best, :boolean
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,18 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170213204946) do
+ActiveRecord::Schema.define(version: 20170217095359) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "answers", force: :cascade do |t|
-    t.integer  "question_id", null: false
-    t.text     "body",        null: false
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.integer  "question_id",                 null: false
+    t.text     "body",                        null: false
+    t.datetime "created_at",                  null: false
+    t.datetime "updated_at",                  null: false
     t.integer  "user_id"
-    t.boolean  "best"
+    t.boolean  "best",        default: false
+    t.index ["question_id", "best"], name: "index_answers_on_question_id_and_best", using: :btree
     t.index ["question_id"], name: "index_answers_on_question_id", using: :btree
     t.index ["user_id"], name: "index_answers_on_user_id", using: :btree
   end
