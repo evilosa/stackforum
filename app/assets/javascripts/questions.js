13d53a97509ac8fa34fe134ce6d5f39f52f023f4
bojkov@mbconsalting.ru <bojkov@mbconsalting.ru>
2017-02-17 11:23:19 +0300

    Рефакторинг кода javascript для обработки вопросов.

--- app/assets/javascripts/questions.js
+++ app/assets/javascripts/questions.js
@@ -2,23 +2,35 @@
 
 document.addEventListener("turbolinks:load", function() {
 
+    function updateForm(params) {
+        $('.edit-form form').attr("method", params.method);
+        $('.edit-form form').attr("action", params.action);
+        $('.bootsy_text_area')[0].name = params.bodyName
+    };
+
     $('#new-answer').on('click', function () {
-        $('.edit-form form').attr("method", "post");
-        $('.edit-form form').attr("action", "/questions/" + $('#new-answer').data('questionId') + "/answers");
-        $('.bootsy_text_area')[0].name = "answer[body]"
+        updateForm({
+            method: "post",
+            action: "/questions/" + $('#new-answer').data('questionId') + "/answers",
+            bodyName: "answer[body]"
+        });
     });
 
     $('#edit-question').on('click', function () {
-        $('.edit-form form').attr("method", "patch");
-        $('.edit-form form').attr("action", "/questions/" + $('#edit-question').data('questionId') +"/update_body" );
-        $('.bootsy_text_area')[0].name = "question[body]"
+        updateForm({
+            method: "patch",
+            action: "/questions/" + $('#edit-question').data('questionId') +"/update_body",
+            bodyName: "question[body]"
+        });
         $('.wysihtml5-sandbox')[0].contentWindow.document.body.innerHTML=$('#question-body').html();
     });
 
     $(document).on('click', '#edit-answer', {}, function () {
-        $('.edit-form form').attr("method", "patch");
-        $('.edit-form form').attr("action", "/questions/" + this.dataset.questionId + "/answers/" + this.dataset.answerId );
-        $('.bootsy_text_area')[0].name = "answer[body]";
+        updateForm({
+            method: "patch",
+            action: "/questions/" + this.dataset.questionId + "/answers/" + this.dataset.answerId,
+            bodyName: "answer[body]"
+        });
         var selector = '#answer-body[data-answer-id="' + this.dataset.answerId + '"]';
         $('.wysihtml5-sandbox')[0].contentWindow.document.body.innerHTML=$(selector)[0].innerHTML;
     });
