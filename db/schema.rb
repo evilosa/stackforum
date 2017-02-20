62c5a7cf7fcfaaad1d150cd356e8dcdd3dc8bce1
bojkov@mbconsalting.ru <bojkov@mbconsalting.ru>
2017-02-20 14:45:51 +0300

    Объединен индекс для вложений.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170220110655) do
+ActiveRecord::Schema.define(version: 20170220114344) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,8 +33,7 @@ ActiveRecord::Schema.define(version: 20170220110655) do
     t.datetime "updated_at",      null: false
     t.integer  "attachable_id"
     t.string   "attachable_type"
-    t.index ["attachable_id"], name: "index_attachments_on_attachable_id", using: :btree
-    t.index ["attachable_type"], name: "index_attachments_on_attachable_type", using: :btree
+    t.index ["attachable_id", "attachable_type"], name: "index_attachments_on_attachable_id_and_attachable_type", using: :btree
   end
 
   create_table "bootsy_image_galleries", force: :cascade do |t|
