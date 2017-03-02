8f55ad9036ec276552e59554e12d93f553d7647b
bojkov@mbconsalting.ru <bojkov@mbconsalting.ru>
2017-03-02 23:43:18 +0300

    Fix questions channel.

--- app/channels/questions_channel.rb
+++ app/channels/questions_channel.rb
@@ -1,9 +1,15 @@
 class QuestionsChannel < ApplicationCable::Channel
   def follow
+    stop_all_streams
     stream_from 'questions'
   end
 
   def follow_question
+    stop_all_streams
     stream_from "question_#{params[:id]}"
+  end
+
+  def unfollow
+    stop_all_streams
   end
 end
