3a633e44976c25d55edb810b8c57b1fc6748c36c
bojkov@mbconsalting.ru <bojkov@mbconsalting.ru>
2017-03-21 15:44:57 +0300

    Create daily digest for user.

--- app/mailers/daily_mailer.rb
+++ app/mailers/daily_mailer.rb
@@ -1,6 +1,8 @@
 class DailyMailer < ApplicationMailer
 
   def digest(user)
-
+    @user = user
+    @questions = Question.where('created_at > ?', 1.days.ago)
+    mail(to: @user.email, subject: 'StackForum. Daily updates.')
   end
 end
