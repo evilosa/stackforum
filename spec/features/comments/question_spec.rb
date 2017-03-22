006b47720f5a7ad58e31e40eacc66a8683228c07
bojkov@mbconsalting.ru <bojkov@mbconsalting.ru>
2017-03-22 11:18:35 +0300

    Fix test errors.

--- spec/features/comments/question_spec.rb
+++ spec/features/comments/question_spec.rb
@@ -4,7 +4,7 @@ feature 'Create comments for the question', %q{
   In order to comment question
   As an user
   I'd like to be able to create comment
-}, driver: :webkit do
+}, driver: :selenium do
 
   given!(:user) { create(:user) }
   given!(:question) { create(:question) }
