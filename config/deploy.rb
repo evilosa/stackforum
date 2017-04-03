f0f4181ab23b3b1391cc414ef005536c2274d460
mbconsalting <bojkov@mbconsalting.ru>
2017-04-03 22:35:38 +0300

    Fix deploy file.

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,7 +9,7 @@ set :deploy_to, '/home/deployer/stackforum'
 set :deploy_user, 'deployer'
 
 # Default value for :linked_files is []
-append :linked_files, 'config/database.yml', 'config/secrets.yml', '.env'
+append :linked_files, 'config/database.yml', 'config/secrets.yml', '.env', 'config/sidekiq.yml', 'config/production.sphinx.conf', 'config/thinking_sphinx.yml'
 
 # Default value for linked_dirs is []
 append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'public/system', 'vendor/bundle', 'public/uploads'
