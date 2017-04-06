11a7c4f1251dacda00b1c6897731f040df57a542
bojkov@mbconsalting.ru <bojkov@mbconsalting.ru>
2017-04-06 12:42:51 +0300

    Change collection.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,11 +10,4 @@ module ApplicationHelper
     def active_action?(action_name)
         params[:action] == action_name ? "active" : nil
     end
-
-  def collection_cache_key_for(model)
-    klass = model.to_s.capitalize.constantize
-    count = klass.count
-    max_updated_at = klass.maximum(:updated_at).try(:utc).try(:to_s, :number)
-    "#{model.to_s.pluralize}/collection-#{count}-#{max_updated_at}"
-  end
 end
